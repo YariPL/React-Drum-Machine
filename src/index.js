1c77c8f0c7aaea8308e3cc82baa36ea4d3d6cab1
Yari PL <yaroslav.khudchenko@gmail.com>
2019-01-19 11:54:02 +0100

    initial app state, 3 classes

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+      	<div id="drum-machine">
+      		<Panel />
+      		<Controls />
+      	</div>
+      </div>
+    );
+  }
+}
+class Panel extends React.Component {
+	render() {
+		return(	
+			<div id="padsPanel">
+
+			</div>
+		)
+	}
+}
+
+class Controls extends React.Component {
+	render() {
+		return(
+			<div id="display">
+				fawfwa
+			</div>
+		)
+	}
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
